90a5806075d66380b996c21ef02f9cba1d9c2944
d1scob4ll <wizlanaya@gmail.com>
2024-03-27 17:14:56 +0300

    crossovers and mutation added

--- src/mutations.py
+++ src/mutations.py
@@ -10,3 +10,22 @@ def square_mutation(coordinates: Sequence[float], delta: float) -> Sequence[floa
     new_coordinates = new_coordinates + distance
 
     return new_coordinates
+
+def coruier_mutation(courier, num_points_to_move=1, random_state=None):
+    if random_state is not None:
+        np.random.seed(random_state)
+    
+    num_couriers = len(courier)
+    
+    for _ in range(num_points_to_move):
+        donor_idx = np.random.choice([i for i, points in enumerate(courier) if len(points) > 1])
+        point_idx = np.random.randint(len(courier[donor_idx]))
+        point_to_move = courier[donor_idx].pop(point_idx)
+        
+        recipient_idxs = list(range(num_couriers))
+        recipient_idxs.remove(donor_idx)
+        recipient_idx = np.random.choice(recipient_idxs)
+        
+        courier[recipient_idx].append(point_to_move)
+
+    return courier
--- src/state.py
+++ src/state.py
@@ -27,3 +27,39 @@ class GeneticAlgorithmState:
     @property
     def population(self):
         return self._population
+    
+class CourierPopulation:
+    def __init__(
+        self, num_couriers: int, 
+        num_points: int, 
+        population_size: int,
+        random_state: int = None
+        
+    ):
+        self.num_couriers = num_couriers
+        self.num_points = num_points
+        self.population_size = population_size
+        self.random_state = random_state
+
+        np.random.seed(random_state)
+
+        if self.num_points < self.num_couriers:
+            raise ValueError("Количество пунктов должно быть не меньше количества курьеров.")
+
+        self._population = []
+        for _ in range(self.population_size):
+
+            points = np.arange(1, self.num_points + 1)
+            np.random.shuffle(points)
+
+            distribution = [[point] for point in points[:self.num_couriers]]
+
+            remaining_points = points[self.num_couriers:]
+            for point in remaining_points:
+                distribution[np.random.randint(0, self.num_couriers)].append(point)
+
+            self._population.append(distribution)
+
+    @property
+    def population(self):
+        return self._population
